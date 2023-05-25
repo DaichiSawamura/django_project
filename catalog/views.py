1d0d8346c4de4ed7e4e14b9fd5d79afa8784ba68
DaichiSawamura <dimakr-303@yandex.ru>
2023-05-25 16:20:18 +0300

    add

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Product.objects.all()
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+
+def product(request, pk=None):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'title': product_item.name,
+        'desc': product_item.description,
+        'category': product_item.category,
+        'price': product_item.price,
+        'create_date': product_item.create_date,
+        'change_date': product_item.change_date,
+    }
+    return render(request, 'catalog/product.html', context)
