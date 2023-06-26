fb973dffee2d6e1930212360530b7d829386675b
DaichiSawamura <dimakr-303@yandex.ru>
2023-06-26 17:29:00 +0300

    fix

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from users.views import ProfileUpdateView, RegisterView
+from users.views import ProfileUpdateView, RegisterView, generate_new_password
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileUpdateView.as_view(), name='profile'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('profile/gennpassword', generate_new_password, name='generate_new_password'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,16 @@
+import secrets
+
+from django.conf import settings
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 from django.views.generic import UpdateView, CreateView
 from users.models import User
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from users.forms import UserForm, UserRegisterForm
+from users.services import confirm_account
 
 
 class ProfileUpdateView(UpdateView):
-
     model = User
     form_class = UserForm
     success_url = reverse_lazy('users:profile')
@@ -15,7 +20,35 @@ class ProfileUpdateView(UpdateView):
 
 
 class RegisterView(CreateView):
-
     model = User
     form_class = UserRegisterForm
-    success_url = reverse_lazy('catalog:index')
+    success_url = f'/users/after_registration/'
+
+    def form_valid(self, form):
+        if form.is_valid():
+            self.object = form.save()
+            self.object.is_active = False
+            self.object.token = secrets.token_urlsafe(18)[:15]
+            confirm_account(self.object)
+            self.object.save()
+            self.user_token = self.object.token
+            self.object = form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        new_url = super().get_success_url()
+        token = self.object.token
+        return str(new_url) + str(token)
+
+
+def generate_new_password(request):
+    pass_ch = secrets.token_urlsafe(18)[:9]
+    send_mail(
+        subject='Смена пароля',
+        message=f'Ваш новый пароль {pass_ch}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email]
+    )
+    request.user.set_password(pass_ch)
+    request.user.save()
+    return redirect(reverse('users:login'))
