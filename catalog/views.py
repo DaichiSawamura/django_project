353e3744b7b51e265f9a868f9325090fa3e6c080
DaichiSawamura <dimakr-303@yandex.ru>
2023-07-05 16:12:17 +0300

    fix and add

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.forms import inlineformset_factory
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404, reverse
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
-from catalog.models import Product, Record
+
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Product, Record, Category, Version
+from catalog.services import get_category_subjects
 
 
 # Create your views here.
@@ -13,10 +19,24 @@ class IndexView(TemplateView):
     }
 
 
+class CategoriesListView(ListView):
+    model = Category
+    extra_context = {
+        'title': 'Все категории',
+        'object_list': Category.objects.all()
+    }
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['subject_list'] = get_category_subjects
+        return context_data
+
+
 class ProductListView(ListView):
     model = Product
     extra_context = {
-        'object_list': Product.objects.all(),
+        'object_list': Product.objects.filter(status='Активна'),
+        'version_list': Version.objects.filter(sign_of_publication=True),
         'title': 'Все продукты'
     }
 
@@ -58,6 +78,12 @@ class RecordUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('catalog:record_detail', args=[str(self.object.slug)])
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        if self.object.user != self.request.user:
+            raise Http404("Вы не являетесь владельцем этого товара")
+        return self.object
+
 
 class RecordDeleteView(DeleteView):
     model = Record
@@ -66,6 +92,60 @@ class RecordDeleteView(DeleteView):
 
 class ProductDetailView(DetailView):
     model = Product
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = self.get_object()
+        return context_data
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:products_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form_with_formset.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        SubjectFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = SubjectFormset(self.request.POST)
+        else:
+            context_data['formset'] = SubjectFormset()
+        return context_data
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:products_list')
+
+
+class VersionListView(ListView):
+    model = Version
+    extra_context = {
+        'object_list': Version.objects.filter(sign_of_publication=True),
+        'title': 'Все версии'
+    }
+
+
+class VersionUpdateView(UpdateView):
+    model = Version
+    form_class = VersionForm
+    success_url = reverse_lazy('catalog:products_list')
+
+
+class VersionDetailView(DetailView):
+    model = Version
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
